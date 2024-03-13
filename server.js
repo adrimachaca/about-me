5916e1bc226913b0a965501f6e6f6e6ddccf3a11
adrimachaca <adrimachacalopez@gmail.com>
2024-03-13 13:09:25 +0100

    modificaciones en el server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+//Imports
 import express from "express";
 import "dotenv/config"; // import dotenv
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
+//Variables
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static("public"));
 
