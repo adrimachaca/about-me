72767e45e8e2948d9d8ab77e0c8dce439cc2bc91
adrimachaca <adrimachacalopez@gmail.com>
2024-03-13 13:34:29 +0100

    creation of index.html

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
